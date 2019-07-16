4b901c73da47f98bd52582f2e9dbf60ea07d2d05
bl <bl@hell.office.timeweb.ru>
2019-07-16 04:22:27 +0300

    20190716

---
+++ info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ip="$1"
+export SSHPASS=$2
+if [ -n "$3" ];then
+    user=$3
+else
+    user=root
+fi
+
+c1="pwd"
+c3="cat /etc/os-release|grep '^NAME='"
+
+c13="bash"
+
+#listc=$(egrep "^c\w+=" $0 | sed -e 's/^.*=//g' -e 's/"//g' | sed 's/$/;/g') #| sed ':a; /$/N; s/\n/;/; ta' | sed 's/^/"/;s/$/"/')
+
+listc=$(egrep "^c\w+=" $0 | awk -F"\"" '{ print $2 }' | sed 's/"//g' | sed ':a; /$/N; s/\n/;/; ta')
+sshpass -e ssh -oStrictHostKeyChecking=no $user@$ip -t $listc
